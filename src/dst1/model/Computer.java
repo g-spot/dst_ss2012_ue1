bdd74d38d265a8470391d639a2e4f2f8b40702c2
Gerhard Schraml <g.schraml@gmx.at>
2012-03-21 09:43:14 +0100

    completed xml mapping for computer

--- src/dst1/model/Computer.java
+++ src/dst1/model/Computer.java
@@ -30,4 +30,68 @@ public class Computer {
 	public Computer() {
 		this.executionList = new ArrayList<Execution>();
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCpus() {
+		return cpus;
+	}
+
+	public void setCpus(int cpus) {
+		this.cpus = cpus;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getCreation() {
+		return creation;
+	}
+
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public List<Execution> getExecutionList() {
+		return executionList;
+	}
+
+	public void setExecutionList(List<Execution> executionList) {
+		this.executionList = executionList;
+	}
+
+	public Cluster getCluster() {
+		return cluster;
+	}
+
+	public void setCluster(Cluster cluster) {
+		this.cluster = cluster;
+	}
 }
